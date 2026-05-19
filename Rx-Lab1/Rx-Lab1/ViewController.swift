6fea166f0f795930e51a96438c7bae9449bca8a4
esraaehab333 <esraaehabb333@gmail.com>
2026-05-19 23:08:08 +0200

    lab 1

---
+++ Rx-Lab1/Rx-Lab1/ViewController.swift
@@ -0,0 +1,46 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var switchbtn: UISwitch!
+    @IBOutlet weak var statusLabel: UILabel!
+    private let disposeBag = DisposeBag()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let observable1: Observable<Int> = Observable<Int>.just(1)
+        let observable2: Observable<String> = Observable<String>.of("Esraa", "Mona", "Hend", "Tasneem")[[],[]]
+        let observable3: Observable<String> = Observable<String>.from(["Esraa", "Mona", "Hend", "Tasneem"])//flat
+        let observable4: Observable<String> = Observable.create {
+            observer in
+            observer.onNext("Esraa")
+            observer.onNext("Mai")
+            observer.onCompleted()
+            observer.onNext("Hend")
+            return Disposables.create()
+        }
+        observable1.subscribe(
+            onNext: { value in
+                print("observable1: \(value)")
+            }).disposed(by: disposeBag)
+        observable2.subscribe(
+            onNext: { value in
+                print("observable2: \(value)")
+            }).disposed(by: disposeBag)
+        observable3.subscribe(
+            onNext: { value in
+                print("observable3: \(value)")
+            }).disposed(by: disposeBag)
+        observable4.subscribe(
+            onNext: { value in
+                print("observable4: \(value)")
+            }).disposed(by: disposeBag)
+        switchbtn.rx.isOn.subscribe(
+            onNext: { isOn in // wek self
+                self.statusLabel.text = isOn ? "On" : "Off"
+            },onCompleted: {
+                print("switch complete")
+            }).disposed(by: disposeBag)
+    }
+}
